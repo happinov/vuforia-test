85773980fc01a26153b981b6e768b4f2e5c5a666
Nicolas Leclerc <nl@spirotron.fr>
2021-05-21 16:55:38 +0200

    Working on xcode 12.

--- www/js/index.js
+++ www/js/index.js
@@ -1,94 +1,101 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
+var LICENSE = 'AfLI8Qb/////AAAAAFJb+k2ck0oolfb8596q1HuLWZNVTieLTx5YVS6VSGgO50Fj0bG91TnhaIr/WXf0Kb9LVqfgqcNGeWkb32GVXL/D5PcgBAFF2Qh5R8YBEiWE4pj83i2CxWzuFKPg+arr5d2mr4dR9jS0MbaI3K1CpymxGpBF/27HrDWyFUzDBoHBMjRn0A4pXzcGOPCgOvoXTdolwb/2YbS+0hWMoCiYfZolqKqoqO5vuNREeH4DczBtWrz3F1CsdswFgDDI3V4m+1x/EU8ESE1qgH1WsFC1WI/XOOyXZ1k7vYAe31rcc240Ys7vNj2VddK7EKod/k16w0EHLGHOz4nJQHFDk3Jw0vQ5h8NbGwxeN0Yjs3qpv7Cg'
+
+function getRootFolderPath() {
+	if (cordova.platformId == "android")
+		return cordova.file.externalApplicationStorageDirectory;
+	else
+		return cordova.file.documentsDirectory;
+}
+
+function getPath(path) {
+	path = path.replace(/^\//,'');
+	return getRootFolderPath()+path;
+}
+
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-        console.log("L'appareil est prêt");
-        screen.lockOrientation('landscape');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+	initialize: function() {
+		this.bindEvents();
+	},
+	bindEvents: function() {
+		document.addEventListener('deviceready', this.onDeviceReady, false);
+	},
+	onDeviceReady: function() {
+		console.log("L'appareil est prêt");
+		screen.lockOrientation('landscape');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+		function startAR() {
+			console.log("VUFORIA START AR!");
+			var folderPath = getPath('StonesAndChips');
+//			 var folderPath = getPath('plugintest4');
 
-        console.log('Received Event: ' + id);
-    }
+			var options = {
+				vuforiaLicense: LICENSE,
+				overlayImageFolder: folderPath,
+				databaseXmlFile: folderPath+'/database.xml'
+			};
+
+			Vuforia.openFinder(options,function(){/* do nothing */}, function(err){
+				alert("Error opening Vuforia:\n"+err)
+			});
+		}
+
+		function startIR() {
+			console.log("VUFORIA START IR!");
+			console.log(cordova.platformId);
+
+			var absolutePath = getPath('plugintest3/database.xml');
+
+			console.log('Le chemin absolu est', absolutePath);
+
+			var options = {
+				vuforiaLicense: LICENSE,
+				targetList: [ 'clubmed','tron' ],
+				databaseXmlFile: absolutePath,
+			};
+
+			Vuforia.openIRFinder(options,
+				function(data){
+					console.log('RESULT:',data);
+					Vuforia.closeFinder();
+					// alert("Image found: " + data.imageName);
+				},
+				function(err){
+					console.error('ERROR:',err);
+				}
+			);
+		};
+
+		var currentFunction;
+		currentFunction = startAR;
+		// currentFunction = startIR;
+
+		document.getElementById("vuforia_button").onclick = currentFunction;
+
+		currentFunction();
+
+
+		//
+		// function playSound() {
+		// 	// var soundPathIos = (cordova.file.documentsDirectory) + 'shutter.mp3';
+		// 	var soundPathIos = 'shutter.mp3';
+		// 	var soundPath = soundPathIos;
+		//
+		// 	var media = new Media(soundPath,function(){
+		// 		console.log('Sound played.');
+		// 		media.release();
+		// 	}, function (err) {
+		// 		console.error('Error playing sound:',err.message);
+		// 		media.release();
+		// 	});
+		//
+		// 	media.play();
+		// };
+		//
+		// document.getElementById("sound_button").onclick = playSound;
+		//
+		// playSound();
+	}
 };
 
-
-document.getElementById("vuforia_button").onclick = function(){
-    console.log("VUFORIA START!");
-    console.log(cordova.platformId);
-    var absolutePathIos = (cordova.file.documentsDirectory) + 'ARDemoDatabase.xml';
-    var absolutePathAndroid = (cordova.file.externalApplicationStorageDirectory) + 'ARDemoDatabase.xml';
-
-    var absolutePath = (cordova.platformId == "android")?absolutePathAndroid:absolutePathIos;
-
-    console.log('Le chemin absolu est', absolutePath);
-
-    navigator.VuforiaPlugin.startVuforia(
-        {databaseXmlFile: absolutePath,
-        targetList: [ 'test' ],
-        overlayMessage: 'test',
-        vuforiaLicense: 'Abl9cyr/////AAAAASvTXIbXtECnjS63XzT4JMESMOEiIiXhEWlffeXmsWoNhFroMo5NbMYXaVxDqmN1ngOv2dJEltqZeJu5f83wRTt5CqPbHuZB/RhDyf5Ur/Au07YbW8Js+XBX8jgEf06wERNOlxWTjvfo8g6nbfqCaJiDXhnKX0w7qCCrNGJTDowRRPiJydMPbKFrxlUsc1sWwe+wYoL5aQOrR2wKAWjOy/4UVzScR0yQZcsClKzaBMvvClduAxVb8OEwthHJbapu23zE+lrhILyh2d0C4VFQORKfugDcS/WwX3StNeuo1yMrCsFIO+OVJ5APabL7uZeXD10Ou8Zsc8K0WVreKUqhdHPUI2loe6Y19RcIpKHISzSF',
-        showAndroidCloseButton: true,
-        showDevicesIcon: false,
-        // autostopOnImageFound: false
-        },
-        function(data){
-            console.log(data);
-            // alert("Image found: " + data.imageName);
-        },
-        function(err){
-            console.error(err);
-        }
-    );
-
-    // navigator.VuforiaPlugin.startVuforia(absolutePath, [ 'test' ], null,  'Abl9cyr/////AAAAASvTXIbXtECnjS63XzT4JMESMOEiIiXhEWlffeXmsWoNhFroMo5NbMYXaVxDqmN1ngOv2dJEltqZeJu5f83wRTt5CqPbHuZB/RhDyf5Ur/Au07YbW8Js+XBX8jgEf06wERNOlxWTjvfo8g6nbfqCaJiDXhnKX0w7qCCrNGJTDowRRPiJydMPbKFrxlUsc1sWwe+wYoL5aQOrR2wKAWjOy/4UVzScR0yQZcsClKzaBMvvClduAxVb8OEwthHJbapu23zE+lrhILyh2d0C4VFQORKfugDcS/WwX3StNeuo1yMrCsFIO+OVJ5APabL7uZeXD10Ou8Zsc8K0WVreKUqhdHPUI2loe6Y19RcIpKHISzSF',
-    // function(data){
-    //     console.log(data);
-    //     alert("Image found: " + data.imageName);
-    // },
-    // function(err){
-    //     console.error(err);
-    // });
-    //
-
-
-};
 app.initialize();
